40a41d94acacaf246f8779389e8093e1fd35e0f8
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2022-11-29 23:52:36 +0700

    feat: .

--- server/app/app.py
+++ server/app/app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Api
-from resources.food_list_resource import FoodListResource
+from data.fake_food_list_data import getFoodListData
+
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(FoodListResource, '/')
+
+@app.route("/")
+def homepage():
+  return render_template("index.html", foodlist=getFoodListData().json()['foodList'])
+
 
 if __name__ == "__main__":
   app.run(debug=False, host="0.0.0.0", port=8080)
